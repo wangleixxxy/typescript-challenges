4119ee14d9ed3e618294fb89496d493530e284db
wangleixxxy <wangleixxxy@163.com>
2022-06-05 22:52:53 +0800

    Update 2688-medium-startswith.ts

--- medium/2688-medium-startswith.ts
+++ medium/2688-medium-startswith.ts
@@ -15,15 +15,3 @@ type Result2 = StartsWith<'abc', 'ab'>; // true
 type Result3 = StartsWith<'abc', 'abcd'>; // false
 type Result4 = StartsWith<'abc', ''>; // true
 type Result5 = StartsWith<'abc', ' '>; // false
-
-// ----
-/* _____________ 你的代码 _____________ */
-
-type EndsWith<T extends string, U extends string> = T extends `${infer R}${U}`
-  ? true
-  : false;
-
-/* _____________ 测试用例 _____________ */
-type Result6 = EndsWith<'abc', 'bc'>; // true
-type Result7 = EndsWith<'abc', 'abc'>; // true
-type Result8 = EndsWith<'abc', 'd'>; // false
