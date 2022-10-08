70b472457e5bbda103399de72db700c186fec4b2
wangleixxxy <wangleixxxy@163.com>
2022-10-08 14:21:42 +0800

    Create 00459-medium-flatten.ts

---
+++ 00459-medium-flatten.ts
@@ -0,0 +1,26 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils';
+
+type cases = [
+  Expect<Equal<Flatten<[]>, []>>,
+  Expect<Equal<Flatten<[1, 2, 3, 4]>, [1, 2, 3, 4]>>,
+  Expect<Equal<Flatten<[1, [2]]>, [1, 2]>>,
+  Expect<Equal<Flatten<[1, 2, [3, 4], [[[5]]]]>, [1, 2, 3, 4, 5]>>,
+  Expect<
+    Equal<
+      Flatten<[{ foo: 'bar'; 2: 10 }, 'foobar']>,
+      [{ foo: 'bar'; 2: 10 }, 'foobar']
+    >
+  >
+];
+
+// ============= Your Code Here =============
+/**
+ * 遍历数组？
+ * 判断每一项后递归处理
+ */
+type Flatten<T extends any[], U = []> = T extends [infer First, ...infer Rest]
+  ? First extends any[]
+    ? [...Flatten<First>, ...Flatten<Rest>]
+    : [First, ...Flatten<Rest>]
+  : [];
